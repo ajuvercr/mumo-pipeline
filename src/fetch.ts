6c551b752936a33b51749371100650eeeff275a2
ajuvercr <arthur.vercruysse@ugent.be>
2023-06-22 14:26:37 +0200

    init commit

---
+++ src/fetch.ts
@@ -0,0 +1,85 @@
+import { Writer } from "@treecg/connector-types";
+import { readFileSync, writeFileSync } from "fs";
+import fetch, { Headers } from "node-fetch";
+
+interface Link {
+  target: string;
+  url: string;
+}
+
+function extract_links(headers: Headers): Link[] {
+  // link = </ingest/?index=1514>; rel="next", </ingest/?index=1512>; rel="prev"
+  const link = headers.get("link");
+  if (!link) return [];
+
+  const out: Link[] = [];
+
+  for (let l of link.split(",")) {
+    let [key, val] = l.split(";");
+    if (!key || !val) continue;
+
+    const url = key.trim().slice(1, -1);
+    const [val_key, val_val] = val.split("=");
+
+    if (!val_key || !val_val) continue;
+    if (val_key.trim().toLowerCase() !== "rel") continue;
+    const target = val_val.trim().slice(1, -1);
+
+    out.push({ target, url });
+  }
+
+  return out;
+}
+
+async function findNextUrl(url: string, interval_ms: number, maybeLinks?: Link[]): Promise<string> {
+  let links = !!maybeLinks ? maybeLinks : await fetch(url).then(resp => extract_links(resp.headers));
+
+  while (true) {
+    const next = links.find(x => x.target === "next");
+    if (next) {
+      // Found next url
+      const url_url = new URL(url);
+      return `${url_url.protocol}//${url_url.host}${next.url}`;
+    }
+
+    console.log("waiting");
+    // Wait and refetch and look for headers
+    await new Promise(res => setTimeout(res, interval_ms));
+    const resp = await fetch(url);
+    links = extract_links(resp.headers);
+  }
+}
+
+async function start(writer: Writer<string>, start_url: string, interval_ms: number, save_path?: string) {
+  const save = (url: string) => {
+    if (save_path)
+      writeFileSync(save_path, url, { encoding: "utf8" });
+  };
+
+  let url = start_url;
+  if (save_path) {
+    try {
+      url = readFileSync(save_path, { encoding: "utf8" });
+      url = await findNextUrl(url, interval_ms);
+    } catch (ex: any) {
+    }
+  }
+
+  while (true) {
+    console.log("fetching url", url);
+    const resp = await fetch(url);
+    let links = extract_links(resp.headers);
+
+    const text = await resp.text();
+    console.log("got text!", text.length);
+
+    await writer.push(text);
+    save(url);
+
+    url = await findNextUrl(url, interval_ms, links);
+  }
+}
+
+export function fetcher(writer: Writer<string>, start_url: string, save_path?: string, interval_ms = 1000) {
+  start(writer, start_url, interval_ms, save_path);
+}
---
+++ src/mapper.ts
@@ -0,0 +1,133 @@
+import { NamedNode, Quad } from "@rdfjs/types";
+import { Stream, Writer } from "@treecg/connector-types";
+import { createTermNamespace, RDF, XSD } from "@treecg/types";
+import * as N3 from "n3";
+
+const { quad, literal, blankNode } = N3.DataFactory;
+
+const qudt = createTermNamespace("http://qudt.org/1.1/schema/qudt#", "unit", "numericValue", "QuantityValue");
+const qudtUnit = createTermNamespace("http://qudt.org/1.1/vocab/unit#", "DegreeCelsius");
+const sosa = createTermNamespace("https://www.w3.org/ns/sosa/", "Sensor", "observes", "madeBySensor")
+const mumo = createTermNamespace("http://mumo.be/ns/")
+const mumoData = createTermNamespace("http://mumo.be/data/")
+const mumoMeting = createTermNamespace("http://mumo.be/data/")
+const isotc = createTermNamespace("http://def.isotc211.org/iso19156/2011/Observation#",
+  "OM_Observation.resultTime", "OM_Observation.result", "OM_Observation");
+const oslo = createTermNamespace("https://data.vlaanderen.be/ns/observaties-en-metingen#");
+
+
+type Model = {
+  end_device_ids: {
+    device_id: string
+    application_ids: {
+      application_id: string
+    },
+    dev_eui: string,
+    dev_addr: string,
+  },
+  correlation_ids: string[],
+  received_at: string,
+  uplink_message: {
+    f_port: number,
+    f_cnt: number,
+    frm_payload: string,
+    decoded_payload: { version: number } & { [typ: string]: number },
+    rx_metadata: {
+      gateway_ids: {
+        gateway_id: string,
+        eui: string
+      },
+      time: string,
+      timestamp: number,
+      rssi: number,
+      channel_rssi: number,
+      snr: number,
+      uplink_token: string,
+      received_at: string,
+    }[],
+    settings: {
+      data_rate: {
+        lora: {
+          bandwidth: number,
+          spreading_factor: number,
+          coding_rate: string,
+        }
+      },
+      frequency: string,
+      timestamp: number,
+      time: string,
+    },
+    received_at: string,
+    consumed_airtime: string,
+    network_ids: {
+      net_id: string,
+      tenant_id: string,
+      cluster_id: string,
+      cluster_address: string,
+    }
+  }
+};
+
+const typeDict: { [key: string]: NamedNode } = {
+  "temperature": qudtUnit.DegreeCelsius
+};
+
+function observation(data: Model, key: string) {
+  const time = data.uplink_message.rx_metadata[0].time;
+  const subj = mumoData.custom(`${time}/${key}`);
+  const device_id = data.end_device_ids.device_id;
+  const value = data.uplink_message.decoded_payload[key];
+
+  if (!time || !subj || !device_id || !value) {
+    return;
+  }
+
+
+  const quads: Quad[] = [];
+  const resultId = blankNode();
+
+  quads.push(quad(subj, RDF.terms.type, isotc.OM_Observation));
+  quads.push(quad(subj, isotc["OM_Observation.resultTime"], literal(time, XSD.terms.dateTime)));
+  quads.push(quad(subj, isotc["OM_Observation.result"], resultId));
+  quads.push(quad(subj, sosa.madeBySensor, mumoData.custom(`sensor/${key}/${device_id}`)));
+
+  quads.push(quad(resultId, RDF.terms.type, qudt.QuantityValue));
+  quads.push(quad(resultId, qudt.unit, typeDict[key]));
+  quads.push(quad(resultId, qudt.numericValue, literal(value, XSD.terms.custom("float"))));
+
+  return quads;
+}
+
+function do_transform(input: string): string[] {
+  const data: Model = JSON.parse(input);
+  const strings: string[] = [];
+
+  const types = ["temperature"];
+  for (let ty of types) {
+    if (data.uplink_message.decoded_payload[ty]) {
+      const obj = observation(data, ty);
+      if (obj) {
+        const writer = new N3.Writer();
+        strings.push(writer.quadsToString(obj));
+      }
+    }
+  }
+
+  return strings;
+}
+
+export function transform(reader: Stream<string>, writer: Writer<string>) {
+
+  reader.data(async input => {
+    try {
+      const objects = do_transform(input);
+      for (let object of objects) {
+        await writer.push(object);
+      }
+    } catch (e: any) {
+      console.log(e);
+    }
+  });
+
+  reader.on("end", writer.end);
+}
